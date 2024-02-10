72a15f6b36dda2a23a1b4bb4044d84ee1b4c3a70
abdilahmusid <amousaid@gmail.com>
2024-02-10 05:54:27 +0100

    5 ta3 sba7

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: amousaid <amousaid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 21:19:24 by amousaid          #+#    #+#             */
-/*   Updated: 2024/02/09 00:15:36 by amousaid         ###   ########.fr       */
+/*   Updated: 2024/02/10 05:41:22 by amousaid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,42 +21,42 @@ void	ft_print_list(t_list *stack_a)
 	}
 }
 
-void	ft_rev_swap(t_list **stack_a, char type)
+int find_highest(t_list *stack)
 {
-	ft_rev_rotate(stack_a, type);
-	ft_swap(stack_a, type);
-}
+	int highest;
 
-void	ft_rot_swap(t_list **stack_a, char type)
-{
-	ft_rotate(stack_a, type);
-	ft_swap(stack_a, type);
+	highest = stack->value;
+	while (stack)
+	{
+		if(highest < stack->value)
+			highest = stack->value;
+		stack = stack->next;
+	}
+	return(highest);
 }
 
 void	ft_sort_3(t_list **stack_a)
 {
-	t_list	*head;
-	t_list	*n1;
-	t_list	*n2;
+	int highest;
 
-	head = (*stack_a);
-	n1 = (*stack_a)->next;
-	n2 = (*stack_a)->next->next;
-	if ((head->value) > (n1->value) && (n1->value)
-		< (n2->value) && (head->value) > (n2->value))
+	highest = find_highest(*stack_a);
+	if (highest == (*stack_a)->value)
 		ft_rotate(stack_a, 'a');
-	else if ((head->value) > (n1->value) && (n1->value)
-		< (n2->value) && (head->value) < (n2->value))
-		ft_swap(stack_a, 'a');
-	else if ((head->value) < (n1->value) && (n1->value)
-		> (n2->value) && (head->value) > (n2->value))
+	else if (highest == (*stack_a)->next->value)
 		ft_rev_rotate(stack_a, 'a');
-	else if ((head->value) < (n1->value) && (n1->value)
-		> (n2->value) && (head->value) < (n2->value))
-		ft_rev_swap(stack_a, 'a');
-	else if ((head->value) > (n1->value) && (n1->value)
-		> (n2->value) && (head->value) > (n2->value))
-		ft_rot_swap(stack_a, 'a');
+	if (((*stack_a)->value) > ((*stack_a)->next->value))
+		ft_swap(stack_a, 'a');
+}
+void ft_sort_5(t_list **stack_a, t_list **stack_b)
+{
+	ft_push(stack_a, stack_b, 'a');
+	ft_push(stack_a, stack_b, 'a');
+	ft_sort_3(stack_a);
+	if ((*stack_b)->value < (*stack_b)->next->value)
+		ft_swap(stack_b, 'b');
+	ft_push(stack_b, stack_a, 'b');
+	ft_rotate(stack_a, 'a');
+	ft_push(stack_b, stack_a, 'b');	
 }
 
 int	main(int argc, char **argv)
@@ -65,12 +65,14 @@ int	main(int argc, char **argv)
 	static char	*numbers;
 	static char	**num_split;
 	t_list		*stack_a;
+	t_list		*stack_b;
 	char		*tmp;
 
 	if (argc < 2)
 		return (ft_printf("[YOU NEED MORE ARGUMENT]\n"));
 	i = 1;
 	stack_a = NULL;
+	stack_b = NULL;
 	while (i < argc)
 	{
 		tmp = numbers;
@@ -83,9 +85,19 @@ int	main(int argc, char **argv)
 		return (ft_printf("[ERROR IN ARGUMENT]\n"));
 	}
 	num_split = ft_split(numbers, ' ');
-	if (ft_split_to_node(num_split, 0, NULL, &stack_a) > 0)
-		return (ft_free_all_ta3_all(numbers, num_split, &stack_a));
-	// ft_sort_3(&stack_a);
-	ft_print_list(stack_a);
+	if (ft_split_to_node(num_split, 0, NULL, &stack_a) == 0)
+	{
+		if (ft_lstsize(stack_a)==2)
+		{
+			ft_swap(&stack_a, 'a');
+			ft_print_list(stack_a);
+			return (0);
+		}
+		else if(ft_lstsize(stack_a)==3)
+			ft_sort_3(&stack_a);
+		else
+			ft_sort_5(&stack_a, &stack_b);
+		ft_print_list(stack_a);
+	}
 	ft_free_all_ta3_all(numbers, num_split, &stack_a);
 }
